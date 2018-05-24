e4fa376723c194f46211668f12259eff5597cf83
Wenwen Xie <wxie11@hawk.iit.edu>
2018-05-24 13:30:35 -0500

    add #3 solution

--- eloquent_javascript/ch2.js
+++ eloquent_javascript/ch2.js
@@ -1,12 +1,10 @@
 //Looping a triangle
-
 for (let x = "#"; x.length < 8; x += "#")
 {
 	console.log(x);
 }
 
 //FizzBuzz
-
 for(let num = 1; num <= 100; num++)
 {
   let output = "";
@@ -14,3 +12,23 @@ for(let num = 1; num <= 100; num++)
   if(num % 5 === 0) output += "Buzz";
   console.log(output || num);
 }
+
+// Chessboard
+let output = "";
+for (let i = 0; i < 8; i++)
+{
+  for (let j = 0; j < 8; j++)
+  {
+    if((i+j) %2 === 0)
+    {
+      output += " ";
+    }
+    else
+    {
+      output += "#";
+    }
+  }
+  output += "\n";
+}
+
+console.log(output);
