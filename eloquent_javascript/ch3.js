6aef78fe53625527f4a9ed1819de1f1c5ff36eca
Wenwen Xie <wxie11@hawk.iit.edu>
2018-05-24 13:43:05 -0500

    add ch3 #2,3 solutions

--- eloquent_javascript/ch3.js
+++ eloquent_javascript/ch3.js
@@ -8,3 +8,41 @@ console.log(min(0, 10));
 // → 0
 console.log(min(0, -10));
 // → -10
+
+//Recursion
+function isEven(x) {
+  if(x === 0) return true;
+  else if(x === 1) return false;
+  else if(x < 0) return isEven(-x);
+  else return isEven(x - 2);
+}
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → false
+
+//Bean Counting
+function countChar(string, char) {
+  let count = 0;
+  for(let i = 0; i < string.length; i++)
+  {
+    if(string[i] === char)
+    {
+    	count ++;
+    }
+  }
+  return count;
+}
+
+function countBs(string) {
+  return countChar(string, "B");
+}
+
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
