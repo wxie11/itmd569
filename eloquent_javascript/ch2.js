27bc38be6daa58d1671b1ed46a90f65e7573640d
Wenwen Xie <wxie11@hawk.iit.edu>
2018-05-24 13:15:14 -0500

    add #2 solution

--- eloquent_javascript/ch2.js
+++ eloquent_javascript/ch2.js
@@ -4,3 +4,13 @@ for (let x = "#"; x.length < 8; x += "#")
 {
 	console.log(x);
 }
+
+//FizzBuzz
+
+for(let num = 1; num <= 100; num++)
+{
+  let output = "";
+  if(num % 3 === 0) output += "Fizz";
+  if(num % 5 === 0) output += "Buzz";
+  console.log(output || num);
+}
