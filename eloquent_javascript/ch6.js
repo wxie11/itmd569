6c05b11344ea46f74972b3945ab16b05f05a893e
Wenwen Xie <wxie11@hawk.iit.edu>
2018-05-24 22:01:34 -0500

    add ch6 #2 solution

--- eloquent_javascript/ch6.js
+++ eloquent_javascript/ch6.js
@@ -24,3 +24,42 @@ console.log(new Vec(1, 2).minus(new Vec(2, 3)));
 // → Vec{x: -1, y: -1}
 console.log(new Vec(3, 4).length);
 // → 5
+
+//Groups
+class Group {
+  constructor() {
+  	this.elements = [];
+  }
+
+  add(value) {
+    if(!this.has(value)) {
+      this.elements.push(value);
+    }
+  }
+
+  delete(value) {
+    this.elements = this.elements.filter(v => v !== value);
+  }
+
+  has(value) {
+    return this.elements.includes(value);
+  }
+
+  static from(collection) {
+    let group = new Group;
+    for (let value of collection) {
+      group.add(value);
+    }
+    return group;
+  }
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
