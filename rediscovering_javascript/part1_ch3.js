490892c728cb582e0f3198aaf7b18c229ad24c57
Wenwen Xie <wxie11@hawk.iit.edu>
2018-05-28 20:30:22 -0500

    add ex3 solution

--- rediscovering_javascript/part1_ch3.js
+++ rediscovering_javascript/part1_ch3.js
@@ -41,3 +41,16 @@ const mustHaves = ['bread', 'milk'];
 const andAlso = ['eggs', 'donuts', 'tea'];
 
 purchaseItems(...mustHaves, ...andAlso);
+
+//Exercise 3
+'use strict';
+
+const purchaseItems = function(essential1 = 'milk', essential2 = 'bread', ...optionals)
+{
+  console.log(essential1 + ', ' + essential2 + ', ' + optionals.join(', '));
+};
+
+const items = ['cheese', 'milk'];
+purchaseItems('cheese'); //cheese, bread,
+purchaseItems(...items); //cheese, milk,
+purchaseItems(); //milk, bread,
